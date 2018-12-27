010e5035cadace4e2a347caeec739aaa15a0364c
Peter <45145046+afghWp@users.noreply.github.com>
2018-12-27 23:00:05 +0100

    Update orderZamowienie.php
    kolejna zmiana

--- orderZamowienie.php
+++ orderZamowienie.php
@@ -15,7 +15,12 @@
 		$i_p = $_POST['i_p'];
 		$i_g = $_POST['i_g'];
 		
-		echo "Ala".$i_p."  ".$i_g;
+		$w_p = $i_p * 0.99;
+		$w_g = $i_g * 1.23;
+		
+		$suma = $w_p + $w_g;
+		
+		echo "<h2> Wartość pączków $w_p </h2>";
 ?>
 
 </body>
